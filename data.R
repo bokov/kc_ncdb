4702d8faf999810c1e296ccda235d127e813a558
Alex F. Bokov <bokov@uthscsa.edu>
2018-10-22 16:35:42 -0500

    data.R;bugfix;Resolved consequences of splitting out special values from numeric columns into separate special columns.

--- data.R
+++ data.R
@@ -9,6 +9,7 @@
 #' 
 # init -------------------------------------------------------------------------
 source('global.R');
+options(gitstamp_prod=F);
 .depends <- 'dictionary.R';
 .depdata <- paste0(.depends,'.rdata');
 .currentscript <- parent.frame(2)$ofile;
@@ -43,9 +44,14 @@ for(.ii in intersect(levels_map$varname,v(c_donotmap))){
   # so far all the special levels of 0 amount to a numeric value of 0, but this 
   # might need to be handled differently later
   .navals <- setdiff(levels_map[levels_map$varname==.ii,'code'],0);
-  dat1[[paste0(.ii,'_special')]] <- factorclean(dat1[[.ii]],spec_mapper=levels_map
-                                                ,var=.ii,droplevels = T);
-  dat1[[.ii]][dat1[[.ii]] %in% .navals] <- NA;
+  if(any(dat1[[.ii]] %in% .navals)){
+    dat1[[.sii<- paste0(.ii,'_special')]] <- factorclean(dat1[[.ii]]
+                                                         ,spec_mapper=levels_map
+                                                         ,var=.ii
+                                                         ,droplevels = T);
+    dat1[[.ii]][dat1[[.ii]] %in% .navals] <- NA;
+    # blow away the non-special levels in the special version of this variable
+    dat1[[.sii]][!is.na(dat1[[.ii]])]<-NA;}
 }
 #' 
 #' Simplified recurrence type: Not available in NCDB?
@@ -105,20 +111,17 @@ dat2 <- subset(dat1,PUF_CASE_ID %in% sbs0$eligib0) %>%
 # cox ph ----------
 #' Bulk univariate analysis
 .cph0 <- coxph(Surv(DX_LASTCONTACT_DEATH_MONTHS,PUF_VITAL_STATUS=='0: Dead')~1
-               ,data=dat2)
-               # # no renal pelvis
-               # ,subset=PRIMARY_SITE=='C649'&
-               #   # analyze stage IV separately
-               #   ANALYTIC_STAGE_GROUP!='4: Stage IV'&
-               #   # only do the surgical cases
-               #   REASON_FOR_NO_SURGERY=='0: Surgery of the primary site was performed'&
-               #   # only do the surgical cases
-               #   RX_HOSP_SURG_APPR_2010!='0: No surgical procedure of primary site'&
-               #   # only the patients who are presenting with their first ever tumor
-               #   SEQUENCE_NUMBER=='00');
-
-cph_uni <- setdiff(names(dat2),c(v(c_missingmap),v(c_nonanalytic))) %>% 
-  sapply(function(xx) try(update(.cph0,paste0('.~`',xx,'`'))),simplify=F);
+               ,data=dat2);
+.cph0_update <- setdiff(names(dat2),c(v(c_missingmap),v(c_nonanalytic))) %>% 
+  sapply(function(xx) xx[!xx %in% c(v(c_missingnap),v(c_nonanalytic)) && 
+                           mean(is.na(dat2[[xx]])<0.2)&&
+                           #!grepl('_special$',xx)&&
+                           (!grepl(':::',xx)||min(table(dat2[[xx]]))>20)] %>% 
+           sprintf('.~`%s`',.),simplify=F) %>% `[`(.,sapply(.,length)>0);
+cph_uni <- list();
+message('Doing univariate fits');
+for(.ii in names(.cph0_update)) cph_uni[[.ii]]<-update(.cph0
+                                                       ,.cph0_update[[.ii]]);
 cph_uni_tab <- cph_uni[!sapply(cph_uni,is,'try-error')] %>% 
   sapply(function(xx) cbind(tidy(xx),glance(xx)),simplify=F) %>% 
   do.call(bind_rows,.) %>% arrange(desc(concordance)) %>% 
